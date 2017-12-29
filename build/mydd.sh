3ab7174561b87939fc6a8cba3cdc2a46e6f463ec
Flyour <3149321625@qq.com>
2017-12-29 20:56:07 +0800

    第七章第七节

--- build/mydd.sh
+++ build/mydd.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+gcc -I ../lib/kernel/ -I ../lib/ -c -o timer.o ../device/timer.c -m32
+
 gcc -I ../lib/kernel/ -I ../lib/ -I ../kernel/ -c -fno-builtin -o main.o ../kernel/main.c -m32
 
 nasm -f elf -o print.o ../lib/kernel/print.S
@@ -8,8 +10,8 @@ nasm -f elf -o kernel.o ../kernel/kernel.S
 
 gcc -I ../lib/kernel/ -I ../lib/ -I ../kernel/ -c -fno-builtin -o interrupt.o ../kernel/interrupt.c -m32
 
-gcc -I ../lib/kernel/ -I ../lib/ -I ../kernel/ -c -fno-builtin -o init.o ../kernel/init.c -m32
+gcc -I ../lib/kernel/ -I ../lib/ -I ../kernel/ -I ../device/ -c -fno-builtin -o init.o ../kernel/init.c -m32
 
-ld -Ttext 0xc0001500 -e main -o kernel.bin main.o init.o interrupt.o print.o kernel.o -m elf_i386
+ld -Ttext 0xc0001500 -e main -o kernel.bin main.o timer.o  init.o interrupt.o print.o kernel.o -m elf_i386
 
 dd if=kernel.bin of=../hd60M.img bs=512 count=200 seek=9 conv=notrunc
